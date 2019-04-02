160d468e422197918c2f5a3cae6c34c719b07b6b
Chuck <charlesvincentanderson@gmail.com>
2019-04-02 11:52:48 -0400

    added bill_id to items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_01_150351) do
+ActiveRecord::Schema.define(version: 2019_04_02_154937) do
 
   create_table "bills", force: :cascade do |t|
     t.date "date"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_04_01_150351) do
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "bill_id"
   end
 
   create_table "procedures", force: :cascade do |t|
