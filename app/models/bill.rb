a7a4bb091ce62295d2d8e037fc281040e55e6426
Tony Wade <wadant3@gmail.COM>
2019-04-08 02:31:04 +0000

    Working on common surgery page, had to change model to actionrecord

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,6 +1,7 @@
-class Bill < ApplicationRecord
+class Bill < ActiveRecord::Base
   belongs_to  :procedure
   has_many :items
   belongs_to  :facility
 end
 
+#This file has applicationRecord instead of ActiveRecord:: Base 
