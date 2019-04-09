7371dc9f68f1d1e55f66606665516a487ee2e5cc
Chuck <charlesvincentanderson@gmail.com>
2019-04-09 12:09:48 -0400

    added a quick bill input path

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   resources :facilities
-  resources :bills
+  resources :bills do
+    collection do
+      get 'quick_new' => 'bills#quick_new'
+      post 'quick_create' => 'bills#quick_create'
+    end
+  end
   resources :procedures
   resources :items
   resources :admins do
     member do
-      post 'approve_procedure'
-      post 'approve_item'
       post 'approve_bill'
     end
     
