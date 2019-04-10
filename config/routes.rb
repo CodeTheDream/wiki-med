f5c116f7c3feb2445351d06b509c652ed001ec1d
Cami Demery <cami.demery@gmail.com>
2019-04-10 17:23:17 +0000

    formatting changes

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
     end
   end
   
- root "facilities#index"
+ root "static_page#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
